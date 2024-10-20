07fe281a8a01f0c3adc20def7dbdfaf5953dd0e8
appoung <appoung@naver.com>
2024-10-20 23:56:28 +0900

    car road limit eliminate

--- car.py
+++ car.py
@@ -28,8 +28,3 @@ class Car:
         else:
             self.pos[1] -= self.vel[1]
             self.pos[0] -= self.vel[0]
-
-        if self.pos[1] < 100:
-            self.pos[1] = 100
-        elif self.pos[1] + self.height > 500:
-            self.pos[1] = 500 - self.height
