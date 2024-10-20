32faa64d2504b259da2ec7397a820793cbd0ac2c
Jihan <suhoon222@outlook.com>
2024-10-20 22:08:30 +0900

    before merge

--- main.py
+++ main.py
@@ -41,16 +41,7 @@ while running:
         cars[0], cars[1:])
 
     # 출력: 충돌 또는 거리/방향 정보 및 각 방향에 차량이 있는지 여부
-    if collision_status:
-        print(f"충돌 발생!")
-    elif shared_state.nearest_distance:
-        print(f"전방 {shared_state.nearest_distance:.2f}m에 {shared_state.direction_to_nearest}에 차량이 있습니다.")
-        print(f"move_x: {movingAuto('x')}, move_y: {movingAuto('y')}, vel_x: {cars[0].vel[0]}, vel_y: {cars[0].vel[1]}")
 
-        # 각 방향에 차량이 있는지 여부 출력
-        print(f"차량 감지 - 위쪽: {shared_state.car_up}, 아래쪽: {shared_state.car_down}, 왼쪽: {shared_state.car_left}, 오른쪽: {shared_state.car_right}")
-    else:
-        print("감지된 차량 없음")
 
     # 화면 업데이트
     pygame.display.flip()
